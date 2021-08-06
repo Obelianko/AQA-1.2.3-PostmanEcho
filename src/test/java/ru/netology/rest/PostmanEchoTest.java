fb33de8f7268a2e9982108f0afe256b9a53c4059
Obelianko <bianka77@inbox.ru>
2021-08-06 18:49:30 +0300

    test failed

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldCheckRequest(){
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some Data"));
+
+    }
+}
+
